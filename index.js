733d42827ff7a50300b8ba809e0079c743deb24b
Vinit Gupta <vinit.gupta@uolo.com>
2023-05-25 19:38:31 +0530

    modify index

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const path = require('path');
 
 const app = express();
 require('dotenv').config()
-const port = process.env.PORT;
-const hostname = process.env.HOST_NAME;
+const port = process.env.PORT || 3000;
 
 app.get('/model', function(req,res){
-    return res.sendFile('https://raw.githubusercontent.com/thevinitgupta/model-server/main/asset/model.json');
+    const dataJsonUrl = process.env.DATA_JSON_URL;
+  if (dataJsonUrl) {
+    res.redirect(dataJsonUrl);
+  } else {
+    res.status(500).send('DATA_JSON_URL environment variable not set.');
+  }
 })
 app.listen(
     port, 
